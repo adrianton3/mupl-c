a769280e3b7b84db8e8c8a6080da34fa45ea1f31
Adrian Toncean <adrianton3@gmail.com>
2015-06-01 10:01:02 +0200

    Switching to ace editor for the demos

--- examples/transpiler/transpiler.js
+++ examples/transpiler/transpiler.js
@@ -1,8 +1,6 @@
 (function () {
 	'use strict';
 
-	var inTextarea = document.getElementById('in');
-	var outTextarea = document.getElementById('out');
 	var statusDiv = document.getElementById('status');
 
 	var failed;
@@ -15,6 +13,7 @@
 	}
 
 	function err(e) {
+		console.log(e.coords);
 		if (!failed) { statusDiv.classList.add('err'); }
 		statusDiv.innerText = e;
 
@@ -42,12 +41,37 @@
 		return tmpAst;
 	}
 
-	inTextarea.addEventListener('keyup', function (ev) {
-		var ast = getAst(this.value);
-		if (ast) {
-			var rawJs = cps.trT(ast);
-			var prettyJs = js_beautify(rawJs, { indent_size: 2 });
-			outTextarea.value = prettyJs;
+	function setupEditors(options) {
+		var inEditor = ace.edit('in-editor');
+		inEditor.setTheme('ace/theme/github');
+		inEditor.setFontSize(options.fontSize);
+		inEditor.on('input', options.onInput);
+
+		var outEditor = ace.edit('out-editor');
+		outEditor.setTheme('ace/theme/github');
+		outEditor.getSession().setMode('ace/mode/javascript');
+		outEditor.setFontSize(options.fontSize);
+		outEditor.setReadOnly(true);
+		outEditor.$blockScrolling = Infinity;
+
+		return {
+			inEditor: inEditor,
+			outEditor: outEditor
+		};
+	}
+
+	var editors = setupEditors({
+		fontSize: 16,
+		onInput: function () {
+			var code = editors.inEditor.getValue();
+			var ast = getAst(code);
+			if (ast) {
+				var rawJs = cps.trT(ast);
+				var prettyJs = js_beautify(rawJs, { indent_size: 2 });
+				editors.outEditor.setValue(prettyJs, -1);
+			}
 		}
 	});
+
+	editors.inEditor.setValue('(if 1 2 3)', 1);
 })();
